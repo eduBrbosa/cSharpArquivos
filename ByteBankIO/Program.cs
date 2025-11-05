1f3307383853f145b0eb8fa4db0ae2175af85db4
eduBrbosa <d.dbarbosa142@gmail.com>
2025-11-05 11:17:45 -0300

    Entendendo leitura de arquivos e encodign de UTF-8

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,38 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoArquivo = "contas.txt";
+        var numBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+        var buffer = new byte[1024]; //1KB
+        
+
+        while(numBytesLidos != 0)
+        {
+
+            numBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+
+        }
 
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.WriteLine(texto);
+        /*foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }*/
     }
 }
